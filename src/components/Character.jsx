f3edeee82c5090a37ecff55a3649347e21a41768
Anna Pozdnikova <annapozdnikova@annas-macbook-pro.home>
2023-05-14 23:13:56 +0200

    first commit

--- src/components/Character.jsx
+++ src/components/Character.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const Character = ({ name, img, description, heroId }) => {
   return (
-    <Link to={`/comics/${heroId}`}>
+    <Link to={`/comics/${heroId}`} style={{ textDecoration: "none" }}>
       <div className="character-container">
         <h3 className="heroName">{name}</h3>
         <img
@@ -14,14 +14,20 @@ const Character = ({ name, img, description, heroId }) => {
         {description ? (
           <div className="heroDescription">
             <p>{description}</p>
-            <Link to={`/comics/${heroId}`} className="find-out-more">
+            <Link
+              to={`/comics/${heroId}`}
+              style={{ textDecoration: "none" }}
+              className="find-out-more"
+            >
               Find out more in comics
             </Link>
           </div>
         ) : (
           <div className="no-description">
             <p>No description available.</p>
-            Find out more in comics by clicking here!
+            <p className="find-more-no-description">
+              Find out more in comics by clicking here!
+            </p>
           </div>
         )}
       </div>
